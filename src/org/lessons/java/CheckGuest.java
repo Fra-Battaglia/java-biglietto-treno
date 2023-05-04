1f228d2ad49efb46837f4e0872a7e472525805c9
Fra-Battaglia <francescobattaglia03@gmail.com>
2023-05-04 18:23:50 +0200

    guestlist exercise done

---
+++ src/org/lessons/java/CheckGuest.java
@@ -0,0 +1,39 @@
+package org.lessons.java;
+
+import java.util.Scanner;
+
+public class CheckGuest {
+	public static void main(String[] args) {
+		String[] Guests = {
+			"Dua Lipa", 
+			"Paris Hilton", 
+			"Manuel Agnelli", 
+			"J-Ax", 
+			"Francesco Totti", 
+			"Ilary Blasi", 
+			"Bebe Vio", 
+			"Luis", 
+			"Pardis Zarei", 
+			"Martina Maccherone", 
+			"Rachel Zeilic"
+		};
+		
+		Scanner in = new Scanner(System.in);
+		String name;
+		
+		System.out.println("Nome prego:");
+		name = in.nextLine();
+		in.close();
+		
+		for (int i = 0; i < Guests.length; i++) {
+			if (name.equals(Guests[i])) {
+				System.out.println("Entra pure!");
+				
+				return;
+			}
+		}
+		
+		System.out.println("Non puoi entrare!");
+		
+	}
+}
