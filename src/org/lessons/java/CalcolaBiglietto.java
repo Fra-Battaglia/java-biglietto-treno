069020fb6dcd5c9d3f8807b321bab2448677f666
Fra-Battaglia <francescobattaglia03@gmail.com>
2023-05-04 15:35:32 +0200

    train ticket price exercise done

--- src/org/lessons/java/CalcolaBiglietto.java
+++ src/org/lessons/java/CalcolaBiglietto.java
@@ -1,5 +1,41 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class CalcolaBiglietto {
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int numKm;
+		int age;
+		
+		System.out.println("Per Quanti Km vuoi viaggiare?");
+		numKm = in.nextInt();
+		
+		float basePrice = numKm * 0.21f;
+		float price = basePrice;
+		
+		System.out.println("Quanti anni hai?");
+		age = in.nextInt();
+		
+		if (age < 18) {
+			
+			price = basePrice/100*80;
+			System.out.println("Il prezzo del tuo biglietto è di " + price + '€');
+			
+		} else if (age > 65) {
+			
+			price = basePrice/100*60;
+			System.out.println("Il prezzo del tuo biglietto è di " + price + '€');
+			
+		} else {
+			
+			System.out.println("Il prezzo del tuo biglietto è di " + price + '€');
+			
+		}
+		
+		in.close();
+		
+	}
 
 }
